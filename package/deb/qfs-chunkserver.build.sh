410ddeb4f51c1bee4d86a38da49267ceb3c6d21a
alex <alex@debian.(none)>
2012-12-07 00:31:04 -0800

    debian package scripts

---
+++ package/deb/qfs-chunkserver.build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+NAME=qfs-chunkserver
+VERSION=1.0
+QFS_BUILD_ROOT=/tmp/
+SOURCE_DIR=/tmp/
+QFS_INSTALL_PREFIX=/opt/qc/qfs/chunkserver
+
+#PREP
+cd $SOURCE_DIR
+if [ -d qfs ]; then
+    rm -rf qfs
+fi
+git clone https://github.com/quantcast/qfs.git
+
+#BUILD
+cd qfs
+make release
+
+#INSTALL
+sudo rm -rf $QFS_BUILD_ROOT$NAME_$VERSION
+mkdir -p $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/bin
+mkdir -p $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/sbin
+mkdir -p $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/conf
+install -m 755 $SOURCE_DIR/qfs/build/release/bin/chunk* $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/bin
+install -m 644 $SOURCE_DIR/qfs/package/conf/ChunkServer.prp $QFS_BUILD_ROOT$NAME"_"$VERSION$QFS_INSTALL_PREFIX/conf
+
+#ADD DEBIAN/control
+mkdir -p $QFS_BUILD_ROOT$NAME"_"$VERSION/DEBIAN
+cp $SOURCE_DIR/qfs/package/deb/qfs-chunkserver.DEBIAN.control $QFS_BUILD_ROOT$NAME"_"$VERSION/DEBIAN/control
+
+#MODIFY
+sudo chown -R root:root $QFS_BUILD_ROOT$NAME"_"$VERSION
+
+#BUILD DEB
+dpkg-deb --build $QFS_BUILD_ROOT$NAME"_"$VERSION
+mv $QFS_BUILD_ROOT$NAME"_"$VERSION.deb .
