90b265af6d9236b61aaf789588f24f389e5b6007
Jeremy R. Fishman <jfishman@quantcast.com>
2013-02-04 16:50:32 -0800

    update test procedure
    Rename the install script, remove a deprecated script, and update the README.

--- benchmarks/mstress/mstress_install.sh
+++ benchmarks/mstress/mstress_install.sh
@@ -3,8 +3,6 @@
 #
 # $Id$
 #
-# Author: Thilee Subramaniam
-#
 # Copyright 2012 Quantcast Corp.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -28,35 +26,33 @@
 # and untar + unzip them for usage.
 #
 
-if [ -z $1 ] || [[ "$1" = -* ]]
+TAR=${TAR:-"tar"}
+
+if [ x"$BOOTSTRAP" == x ]
+then
+	tarfile="build/benchmarks/mstress.tgz"
+	target="mstress-tarball"
+else
+	tarfile="build/benchmarks/mstress-bootstrap.tgz"
+	target="mstress-bootstrap"
+fi
+
+if [ $# -lt 1 ]
 then
 	echo "Usage: $0 <comma-separated hosts>"
   echo "  This copies the mstress bundle to master and client hosts."
 	exit
 fi
 
-which tar &>/dev/null
-if [ $? -ne 0 ]
+if [ ! -f "$tarfile" ]
 then
-  echo "tar command not found."
-  exit 1
+	[ -d build ] || mkdir build
+	(cd build && cmake ../../.. && make "$target") || exit 1
 fi
 
-script_dir=$(dirname "$0")
-
-cd $script_dir/.. && tar cvfz mstress.tgz mstress
-if [ $? -ne 0 ]
-then
-  echo "failed to create archive."
-  cd -
-  exit 1
-fi
-
-cd -
-for v in `echo "$@"|sed 's/,/ /g'`
+while [ $# -ne 0 ]
 do
-	ssh $v  "rm -rf ~/mstress*"
-  scp $script_dir/../mstress.tgz $v:~
-	ssh $v "tar xvfz mstress.tgz"
+	echo "Deploying mstress tarball to $1"
+	ssh $1 "$TAR xzv || echo >&2 failed to untar on '$1'" < "$tarfile"
+	shift
 done
-
