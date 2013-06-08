2a84eb5de8813df02ff6edd7c625f871ba50180c
Mike Ovsiannikov <movsiannikov@quantcast.com>
2013-06-08 14:57:41 -0700

    Qfs kerberos lib: first reasonably complete but untested kerberos 5 service (server) side implementation / kerberos wrapper. The access from multiple concurrent threads to the KrbService class methods needs to be serialized (this is true in general with most kerberos libs implementations anyway). The shared key as well as reply is with KrbService::Reply() method once kerberos authentication handshake completes. The intention is to use the key as pre-shared secret to "bootstrap" TLS-PSK.

--- src/cc/krb/KrbService.cc
+++ src/cc/krb/KrbService.cc
@@ -1,3 +1,29 @@
+//---------------------------------------------------------- -*- Mode: C++ -*-
+// $Id$
+//
+// Created 2013/05/19
+// Author: Mike Ovsiannikov
+//
+// Copyright 2013 Quantcast Corp.
+//
+// This file is part of Kosmos File System (KFS).
+//
+// Licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Kerberos 5 service side authentication implementation.
+//
+//----------------------------------------------------------------------------
+
 #include "KrbService.h"
 
 #include <krb5/krb5.h>
@@ -20,19 +46,20 @@ public:
           mServer(),
           mKeyTab(),
           mErrCode(0),
+          mOutBuf(),
+          mKeyBlockPtr(0),
           mInitedFlag(false),
           mAuthInitedFlag(false),
           mServiceName(),
           mErrorMsg()
         {}
     ~Impl()
-    {
-        Impl::CleanupSelf();
-    }
+        { Impl::CleanupSelf(); }
     const char* Init(
         const char* inServeiceNamePtr)
     {
         CleanupSelf();
+        mErrCode = 0;
         mErrorMsg.clear();
         mServiceName.clear();
         if (inServeiceNamePtr) {
@@ -45,7 +72,15 @@ public:
         }
         return (mErrCode ? mErrorMsg.c_str() : 0);
     }
-    const char* ApReq(
+    const char* Cleanup()
+    {
+        mErrCode = CleanupSelf();
+        if (mErrCode) {
+            mErrorMsg = ErrToStr(mErrCode);
+        }
+        return (mErrCode ? mErrorMsg.c_str() : 0);
+    }
+    const char* Request(
         const char* inDataPtr,
         int         inDataLen)
     {
@@ -58,24 +93,61 @@ public:
             theData.length = max(0, inDataLen);
             theData.data   = const_cast<char*>(inDataPtr);
             krb5_flags   theReqOptions = { 0 };
-            krb5_ticket* theTicket     = 0;
-            krb5_error_code theErr = krb5_rd_req(
+            // krb5_ticket* theTicket     = 0;
+            mErrCode = krb5_rd_req(
                 mCtx,
                 &mAuthCtx,
                 &theData,
                 mServer,
                 mKeyTab,
                 &theReqOptions,
-                &theTicket
+                0 // &theTicket
             );
-            krb5_free_ticket(mCtx, theTicket);
-            if (! theErr) {
+            // krb5_free_ticket(mCtx, theTicket);
+            if (! mErrCode) {
                 return 0;
             }
-            mErrorMsg = ErrToStr(theErr);
+            mErrorMsg = ErrToStr(mErrCode);
+            CleanupAuth();
         }
         return mErrorMsg.c_str();
     }
+    const char* Reply(
+        const char*& outReplyPtr,
+        int&         outReplyLen,
+        const char*& outSessionKeyPtr,
+        int&         outSessionKeyLen)
+    {
+        outReplyPtr      = 0;
+        outReplyLen      = 0;
+        outSessionKeyPtr = 0;
+        outSessionKeyLen = 0;
+        if (! mInitedFlag || ! mAuthInitedFlag) {
+            mErrCode = KRB5_CONFIG_BADFORMAT;
+            if (mErrorMsg.empty()) {
+                mErrorMsg = ErrToStr(mErrCode);
+            }
+            return mErrorMsg.c_str();
+        }
+        krb5_free_data_contents(mCtx, &mOutBuf);
+        mErrCode = krb5_mk_rep(mCtx, mAuthCtx, &mOutBuf);
+        if (! mErrCode) {
+            if (mKeyBlockPtr) {
+                krb5_free_keyblock(mCtx, mKeyBlockPtr);
+                mKeyBlockPtr = 0;
+            }
+            mErrCode = krb5_auth_con_getkey(mCtx, mAuthCtx, &mKeyBlockPtr);
+            if (! mErrCode) {
+                outReplyPtr = (const char*)mOutBuf.data;
+                outReplyLen = (int)mOutBuf.length;
+                return 0;
+            }
+        }
+        mErrorMsg = ErrToStr(mErrCode);
+        return mErrorMsg.c_str();
+    }
+    int GetErrorCode() const
+        { return mErrCode; }
 
 private:
     string            mKeyTabFileName;
@@ -84,6 +156,8 @@ private:
     krb5_principal    mServer;
     krb5_keytab       mKeyTab;
     krb5_error_code   mErrCode;
+    krb5_data         mOutBuf;
+    krb5_keyblock*    mKeyBlockPtr;
     bool              mInitedFlag;
     bool              mAuthInitedFlag;
     string            mServiceName;
@@ -156,6 +230,7 @@ private:
                 theErr = theCloseErr;
             }
         }
+        krb5_free_data_contents(mCtx, &mOutBuf);
         krb5_free_context(mCtx);
         return theErr;
     }
@@ -163,6 +238,10 @@ private:
     {
         if (! mInitedFlag || ! mAuthInitedFlag) {
             return 0;
+        }
+        if (mKeyBlockPtr) {
+            krb5_free_keyblock(mCtx, mKeyBlockPtr);
+            mKeyBlockPtr = 0;
         }
         mAuthInitedFlag = false;
         return krb5_auth_con_free(mCtx, mAuthCtx);
@@ -193,9 +272,40 @@ KrbService::~KrbService()
 
     const char*
 KrbService::Init(
-        const char* inServeiceNamePtr)
+    const char* inServeiceNamePtr)
 {
     return mImpl.Init(inServeiceNamePtr);
-} 
+}
+
+    const char*
+KrbService::Cleanup()
+{
+    return mImpl.Cleanup();
+}
+
+    const char*
+KrbService::Request(
+    const char* inDataPtr,
+    int         inDataLen)
+{
+    return mImpl.Request(inDataPtr, inDataLen);
+}
+
+    const char*
+KrbService::Reply(
+    const char*& outReplyPtr,
+    int&         outReplyLen,
+    const char*& outSessionKeyPtr,
+    int&         outSessionKeyLen)
+{
+    return mImpl.Reply(
+        outReplyPtr, outReplyLen, outSessionKeyPtr, outSessionKeyLen);
+}
+
+    int
+KrbService::GetErrorCode() const
+{
+    return mImpl.GetErrorCode();
+}
 
 }
--- src/cc/krb/KrbService.h
+++ src/cc/krb/KrbService.h
@@ -1,3 +1,29 @@
+//---------------------------------------------------------- -*- Mode: C++ -*-
+// $Id$
+//
+// Created 2013/05/19
+// Author: Mike Ovsiannikov
+//
+// Copyright 2013 Quantcast Corp.
+//
+// This file is part of Kosmos File System (KFS).
+//
+// Licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// Kerberos 5 service side authentication implementation.
+//
+//----------------------------------------------------------------------------
+
 #ifndef KFS_KRB_SERVICE_H
 #define KFS_KRB_SERVICE_H
 
@@ -10,6 +36,16 @@ public:
     ~KrbService();
     const char* Init(
         const char* inServeiceNamePtr);
+    const char* Cleanup();
+    const char* Request(
+        const char* inDataPtr,
+        int         inDataLen);
+    const char* Reply(
+        const char*& outReplyPtr,
+        int&         outReplyLen,
+        const char*& outSessionKeyPtr,
+        int&         outSessionKeyLen);
+    int GetErrorCode() const;
 private:
     class Impl;
     Impl& mImpl;
