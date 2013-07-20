8bf26a8373c31593e5366345d8815a6deb8d0bd6
Mike Ovsiannikov <movsiannikov@quantcast.com>
2013-07-19 21:17:35 -0700

    Ssl filter test: implement "initiator" -- a "client" that can send to and receive arbitrary data from the "responder".

--- src/cc/devtools/sslfiltertest_main.cc
+++ src/cc/devtools/sslfiltertest_main.cc
@@ -32,6 +32,10 @@
 #include "qcdio/qcdebug.h"
 #include "common/MsgLogger.h"
 #include "common/Properties.h"
+#include "common/kfsdecls.h"
+#include "qcdio/QCUtils.h"
+
+#include <errno.h>
 
 #include <iostream>
 #include <string>
@@ -201,6 +205,282 @@ private:
             const Responder& inResponder);
         Responder& operator=(
             const Responder& inResponder);
+    };
+
+    class Initiator
+    {
+    public:
+        Initiator(
+            int                   inInputFd,
+            int                   inOutputFd,
+            SslFilter::Ctx&       inCtx,
+            const string&         inPsk,
+            const string&         inIdentity,
+            const ServerLocation& inServerLocation,
+            int                   inMaxReadAhead,
+            int                   inMaxWriteBehind,
+            NetManager&           inNetManager)
+            : mConnectionPtr(),
+              mSslFilter(
+                inCtx,
+                inPsk.data(),
+                inPsk.size(),
+                inIdentity.c_str(),
+                0,
+                false // inDeleteOnCloseFlag
+              ),
+              mServerLocation(inServerLocation),
+              mRecursionCount(0),
+              mInputSocket(inInputFd),
+              mOutputSocket(inOutputFd),
+              mInputConnectionPtr(),
+              mOutputConnectionPtr(),
+              mCloseConnectionFlag(false),
+              mMaxReadAhead(inMaxReadAhead),
+              mMaxWriteBehind(inMaxWriteBehind),
+              mNetManager(inNetManager),
+              mInputCB(),
+              mOutputCB(),
+              mNetCB()
+        {
+            QCASSERT(mInputSocket.IsGood());
+            QCASSERT(mOutputSocket.IsGood());
+
+            mInputCB.SetHandler(this, &Initiator::InputHandler);
+            mOutputCB.SetHandler(this, &Initiator::OutputHandler);
+            mNetCB.SetHandler(this, &Initiator::NetHandler);
+            const bool kOwnsSocketFlag = false;
+            const bool kListenOnlyFlag = false;
+            mInputConnectionPtr.reset(new NetConnection(
+                &mInputSocket, &mInputCB, kListenOnlyFlag, kOwnsSocketFlag));
+            mOutputConnectionPtr.reset(new NetConnection(
+                &mOutputSocket, &mOutputCB, kListenOnlyFlag, kOwnsSocketFlag));
+        }
+        ~Initiator()
+        {
+            mInputConnectionPtr->Close();
+            mOutputConnectionPtr->Close();
+        }
+        bool Connect(
+            string* inErrMsgPtr)
+        {
+            const bool theNonBlockingFlag = true;
+            TcpSocket& theSocket          = *(new TcpSocket());
+            const int theErr              = theSocket.Connect(
+                mServerLocation, theNonBlockingFlag);
+            if (theErr && theErr != -EINPROGRESS) {
+                if (inErrMsgPtr) {
+                    *inErrMsgPtr = QCUtils::SysError(-theErr);
+                }
+                KFS_LOG_STREAM_ERROR <<
+                    "failed to connect to server " << mServerLocation.ToString() <<
+                    " : " << QCUtils::SysError(-theErr) <<
+                KFS_LOG_EOM;
+                delete &theSocket;
+                return false;
+            }
+            KFS_LOG_STREAM_DEBUG <<
+                "connecting to server: " << mServerLocation.ToString() <<
+            KFS_LOG_EOM;
+            mConnectionPtr.reset(new NetConnection(&theSocket, &mNetCB));
+            mConnectionPtr->EnableReadIfOverloaded();
+            mConnectionPtr->SetDoingNonblockingConnect();
+            mConnectionPtr->SetMaxReadAhead(mMaxReadAhead);
+            const int kConnectTimeout = 120;
+            mConnectionPtr->SetInactivityTimeout(kConnectTimeout);
+            // Add connection to the poll vector
+            mNetManager.AddConnection(mConnectionPtr);
+            mNetManager.AddConnection(mInputConnectionPtr);
+            mNetManager.AddConnection(mOutputConnectionPtr);
+            return true;
+        }
+        int InputHandler(
+            int   inEventCode,
+            void* inEventDataPtr)
+        {
+            mRecursionCount++;
+            QCASSERT(mRecursionCount >= 1);
+
+            switch (inEventCode) {
+	        case EVENT_NET_READ: {
+                    IOBuffer& theIoBuf = mInputConnectionPtr->GetInBuffer();
+                    QCASSERT(&theIoBuf == inEventDataPtr);
+                    mConnectionPtr->Write(&theIoBuf);
+                    break;
+                }
+
+	        case EVENT_NET_ERROR:
+                    // Fall through
+                case EVENT_INACTIVITY_TIMEOUT:
+                    KFS_LOG_STREAM_ERROR << "input: " <<
+                        (inEventCode == EVENT_INACTIVITY_TIMEOUT  ?
+                            string("input timed out") :
+                            QCUtils::SysError(errno, "")) <<
+                    KFS_LOG_EOM;
+                    mCloseConnectionFlag = true;
+                    mInputConnectionPtr->Close();
+                    mInputConnectionPtr->GetInBuffer().Clear();
+                    break;
+
+	        default:
+                    QCASSERT(!"Unexpected event code");
+                    break;
+            }
+            return FlowControl();
+        }
+        int OutputHandler(
+            int   inEventCode,
+            void* inEventDataPtr)
+        {
+            mRecursionCount++;
+            QCASSERT(mRecursionCount >= 1);
+
+            switch (inEventCode) {
+	        case EVENT_NET_WROTE:
+                    if (mCloseConnectionFlag &&
+                            ! mOutputConnectionPtr->IsWriteReady()) {
+                        mOutputConnectionPtr->Close();
+                    }
+                    break;
+
+	        case EVENT_NET_ERROR:
+                    // Fall through
+                case EVENT_INACTIVITY_TIMEOUT:
+                    KFS_LOG_STREAM_ERROR << "output: " <<
+                        (inEventCode == EVENT_INACTIVITY_TIMEOUT  ?
+                            string("input timed out") :
+                            QCUtils::SysError(errno, "")) <<
+                    KFS_LOG_EOM;
+                    mCloseConnectionFlag = true;
+                    mOutputConnectionPtr->Close();
+                    break;
+
+	        default:
+                    QCASSERT(!"Unexpected event code");
+                    break;
+            }
+            return FlowControl();
+        }
+        int NetHandler(
+            int   inEventCode,
+            void* inEventDataPtr)
+        {
+            mRecursionCount++;
+            QCASSERT(mRecursionCount >= 1);
+
+            switch (inEventCode) {
+	        case EVENT_NET_READ: {
+                    IOBuffer& theIoBuf = mConnectionPtr->GetInBuffer();
+                    QCASSERT(&theIoBuf == inEventDataPtr);
+                    mOutputConnectionPtr->Write(&theIoBuf);
+                    break;
+                }
+	        case EVENT_NET_WROTE:
+                    if (! mConnectionPtr->GetFilter()) {
+                        mConnectionPtr->SetFilter(&mSslFilter);
+                    }
+                    if (mCloseConnectionFlag &&
+                            ! mConnectionPtr->IsWriteReady()) {
+                        mConnectionPtr->Close();
+                    }
+                    break;
+
+	        case EVENT_NET_ERROR:
+                    mConnectionPtr->SetMaxReadAhead(0);
+                    if (mConnectionPtr->IsGood() &&
+                            mConnectionPtr->IsWriteReady()) {
+                        mCloseConnectionFlag = mCloseConnectionFlag ||
+                            ! mConnectionPtr->HasPendingRead();
+                        break;
+                    }
+                    // Fall through
+                case EVENT_INACTIVITY_TIMEOUT:
+                    mConnectionPtr->Close();
+                    mConnectionPtr->GetInBuffer().Clear();
+                    break;
+
+	        default:
+                    QCASSERT(!"Unexpected event code");
+                    break;
+            }
+            return FlowControl();
+        }
+    private:
+        NetConnectionPtr     mConnectionPtr;
+        SslFilter            mSslFilter;
+        ServerLocation const mServerLocation;
+        int                  mRecursionCount;
+        TcpSocket            mInputSocket;
+        TcpSocket            mOutputSocket;
+        NetConnectionPtr     mInputConnectionPtr;
+        NetConnectionPtr     mOutputConnectionPtr;
+        bool                 mCloseConnectionFlag;
+        const int            mMaxReadAhead;
+        const int            mMaxWriteBehind;
+        NetManager&          mNetManager;
+        KfsCallbackObj       mInputCB;
+        KfsCallbackObj       mOutputCB;
+        KfsCallbackObj       mNetCB;
+
+        bool IsOverWriteBehindLimit() const
+        {
+            return (
+                mOutputConnectionPtr->GetNumBytesToWrite() > mMaxWriteBehind);
+        }
+        bool IsInputOverWriteBehindLimit() const
+        {
+            return (mConnectionPtr->GetNumBytesToWrite() > mMaxWriteBehind);
+        }
+        int FlowControl()
+        {
+            if (mRecursionCount > 1) {
+                return 0;
+            }
+            QCASSERT(mRecursionCount >= 1);
+            mConnectionPtr->StartFlush();
+            if (mConnectionPtr->IsGood()) {
+                const int kIoTimeout   = 60;
+                const int kIdleTimeout = 600;
+                mConnectionPtr->SetInactivityTimeout(
+                    mConnectionPtr->IsWriteReady() ?
+                        kIoTimeout : kIdleTimeout);
+                if (mConnectionPtr->IsReadReady()) {
+                    if (IsOverWriteBehindLimit()) {
+                        // Shut down read until client unloads the data.
+                        mConnectionPtr->SetMaxReadAhead(0);
+                    }
+                } else {
+                    if (! mCloseConnectionFlag &&
+                            ! IsOverWriteBehindLimit()) {
+                        // Set read back again.
+                        mConnectionPtr->SetMaxReadAhead(mMaxReadAhead);
+                    }
+                }
+                if (mInputConnectionPtr->IsReadReady()) {
+                    if (IsInputOverWriteBehindLimit()) {
+                        // Shut down read until client unloads the data.
+                        mInputConnectionPtr->SetMaxReadAhead(0);
+                    }
+                } else {
+                    if (! mCloseConnectionFlag &&
+                            ! IsInputOverWriteBehindLimit()) {
+                        // Set read back again.
+                        mInputConnectionPtr->SetMaxReadAhead(mMaxReadAhead);
+                    }
+                }
+            } else {
+                delete this;
+                return 0;
+            }
+            QCASSERT(mRecursionCount >= 1);
+            mRecursionCount--;
+            return 0;
+        }
+    private:
+        Initiator(
+            const Initiator& inInitiator);
+        Initiator& operator=(
+            const Initiator& inInitiator);
     };
 
     SslFilterTest()
