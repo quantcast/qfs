b87e5d31253f9b942659458844114041eaec9afa
Mike Ovsiannikov <movsiannikov@quantcast.com>
2014-01-19 06:12:48 -0800

    Kfsio lib: remove unused typedef from PrngIsaac64.

--- src/cc/kfsio/PrngIsaac64.cc
+++ src/cc/kfsio/PrngIsaac64.cc
@@ -36,15 +36,9 @@ class PrngIsaac64::Impl
 {
 private:
     private:
-    typedef  uint64_t ub8;
-    typedef  int64_t  sb8;
-    typedef  uint32_t ub4;   /* unsigned 4-byte quantities */
-    typedef  int32_t  sb4;
-    typedef  uint16_t ub2;
-    typedef  int16_t  sb2;
-    typedef  uint8_t  ub1;
-    typedef  int8_t   sb1;   /* signed 1-byte quantities */
-    typedef  int      word;  /* fastest type available */
+    typedef uint64_t ub8;
+    typedef uint8_t  ub1;
+    typedef int      word;
 #include "isaac64.c"
 
     bool mInitFlag;
