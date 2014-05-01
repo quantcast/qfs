1684cf1af00b2d1c858f35256f4b0761edf9a6ff
Mike Ovsiannikov <movsiannikov@quantcast.com>
2014-05-01 06:33:28 -0700

    Common lib: add 4 byte version of sync add and fetch.

--- src/cc/common/kfsatomic.cc
+++ src/cc/common/kfsatomic.cc
@@ -32,12 +32,16 @@
 #   ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
 #       define _KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_8_BYTE_OPS
 #   endif
+#   ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+#       define _KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_4_BYTE_OPS
+#   endif
 #endif
 
 namespace KFS
 {
 
 #if defined(_KFS_ATOMIC_USE_MUTEX) || \
+    defined(_KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_4_BYTE_OPS) || \
     defined(_KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_8_BYTE_OPS)
 
 static pthread_mutex_t sKfsAtomicMutex = PTHREAD_MUTEX_INITIALIZER;
@@ -68,18 +72,41 @@ void AtomicUnlock() { KfsAtomicUnlockImpl(); }
 }
 #endif /* _KFS_ATOMIC_USE_MUTEX */
 
+#if defined(_KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_4_BYTE_OPS) || \
+        defined(_KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_8_BYTE_OPS)
+template<typename T>
+    inline static T
+SyncAddAndFetchT(
+    volatile void* inValPtr,
+    T              inInc)
+{
+    KfsAtomicLockImpl();
+    T theRet = *reinterpret_cast<volatile T*>(inValPtr);
+    theRet += inInc;
+    *reinterpret_cast<volatile T*>(inValPtr) = theRet;
+    KfsAtomicUnlockImpl();
+    return theRet;
+}
+#endif
+
+#ifdef _KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_4_BYTE_OPS
+extern "C"
+{
+unsigned int __sync_add_and_fetch_4(
+    volatile void* val, unsigned int inc)
+{
+    return SyncAddAndFetchT(val, inc);
+}
+}
+#endif /* _KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_4_BYTE_OPS */
+
 #ifdef _KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_8_BYTE_OPS
 extern "C"
 {
 long long unsigned int __sync_add_and_fetch_8(
     volatile void* val, long long unsigned int inc)
 {
-    KfsAtomicLockImpl();
-    long long unsigned int ret = *((long long unsigned int*)val);
-    ret += inc;
-    *((long long unsigned int*)val) = ret;
-    KfsAtomicUnlockImpl();
-    return ret;
+    return SyncAddAndFetchT(val, inc);
 }
 }
 #endif /* _KFS_ATOMIC_GCC_IMPLEMENT_ATOMIC_8_BYTE_OPS */
