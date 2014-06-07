13c8161e4bdfb80412f4a788084c6d2e5e291892
Mike Ovsiannikov <movsiannikov@quantcast.com>
2014-06-06 23:07:59 -0700

    Test scripts: create test script to test recovery of partial RS blocks with sparse files.

---
+++ src/test-scripts/recoverytest.sh
@@ -0,0 +1,193 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Created 2014/06/06
+# Author: Mike Ovsiannikov
+#
+# Copyright 2014 Quantcast Corp.
+#
+# This file is part of Kosmos File System (KFS).
+#
+# Licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# Test RS recovery with sparse files by creating sparse file and forcing
+# recovery by deleting chunk files and running file verification, and
+# using admin tool to force recovery of existing chunks.
+
+ulimit -c unlimited
+
+builddir=`pwd`
+toolsdir=${toolsdir-"$builddir"/src/cc/tools}
+metadir=${metadir-"$builddir"/src/cc/meta}
+chunkdir=${chunkdir-"$builddir"/src/cc/chunk}
+devtoolsdir=${devtoolsdir-`dirname "$toolsdir"`/devtools}
+qfstestdir=${qfstestdir-"$builddir"/qfstest}
+clicfg=${clicfg-"$qfstestdir"/client.prp}
+clirootcfg=${clirootcfg-"$qfstestdir"/clientroot.prp}
+maxrecovsize=${maxrecovsize-5242880}
+metaport=${metaport-20200}
+metahost=${metahost-127.0.0.1}
+testblocksize=${testblocksize-29313488}
+
+valgrind_cmd=
+if [ x"$1" = x'-valgrind' ]; then
+    valgrind_cmd='valgrind -v --log-file=valgrind.log --leak-check=full --leak-resolution=high --show-reachable=yes --track-origins=yes --'
+    shift
+fi
+
+if [ x"$1" = x'-start' ]; then
+    cd "$qfstestdir"/meta || exit
+    kill -TERM `cat metaserver.pid`
+    rm -f kfscp/* kfslog/*
+    "$metadir"/metaserver -c MetaServer.prp > metaserver.log 2>&1 &
+    echo $! > metaserver.pid
+    cd ../..
+    i=20400
+    while [ $i -le 20401 ]; do
+        cd "$qfstestdir"/chunk/$i || exit
+        kill -TERM `cat chunkserver.pid`
+        rm -f chunkserver.log
+        rm -rf kfschunk*/*
+        sed -i '' \
+            -e 's/^\(chunkServer.diskIo.crashOnError.*\)$/# \1/' \
+            -e 's/^\(chunkServer.rsReader.maxRecoverChunkSize\).*$/\1='"$maxrecovsize"'/' \
+            ChunkServer.prp
+        if grep 'chunkServer.rsReader.maxRecoverChunkSize' ChunkServer.prp \
+                >/dev/null; then
+            true;
+        else
+            echo "chunkServer.rsReader.maxRecoverChunkSize=$maxrecovsize" \
+                >> ChunkServer.prp
+        fi
+        $valgrind_cmd "$chunkdir"/chunkserver ChunkServer.prp > chunkserver.log 2>&1 &
+        echo $! > chunkserver.pid
+        cd ../../..
+        i=`expr $i + 1`
+    done
+fi
+
+usr=`id -un`
+[ -f "$clicfg"     ] || clicfg=/dev/null
+[ -f "$clirootcfg" ] || clirootcfg=/dev/null
+
+"$toolsdir"/qfs \
+    -cfg "$clicfg" \
+    -mkdir "qfs://$metahost:$metaport/user/$usr"
+
+"$toolsdir"/qfs \
+    -cfg "$clicfg" \
+    -rm -skipTrash "qfs://$metahost:$metaport/user/$usr/testrep*.dat"
+
+"$devtoolsdir"/rand-sfmt -g 1 1234 \
+    | "$toolsdir"/qfs \
+        -cfg "$clicfg" \
+        -D fs.createParams=1,6,3,1048576,2,15,15 \
+        -put - "qfs://$metahost:$metaport/user/$usr/testrep1.dat" || exit
+
+"$devtoolsdir"/rand-sfmt -g 29313488 1234 \
+    | "$toolsdir"/qfs \
+        -cfg "$clicfg" \
+        -D fs.createParams=1,6,3,1048576,2,15,15 \
+        -put - "qfs://$metahost:$metaport/user/$usr/testrep.dat" || exit
+
+"$toolsdir"/qfsshell \
+    -f "$clicfg" -s $metahost -p $metaport -q -- \
+    append "/user/$usr/testrep1.dat" "/user/$usr/testrep.dat" || exit
+
+fenumout="$qfstestdir/fenum.txt"
+"$toolsdir"/qfsfileenum -s $metahost -p $metaport -c "$clicfg" \
+        -f "/user/$usr/testrep.dat" > "$fenumout"
+
+cat "$fenumout"
+
+tmpchunk="$qfstestdir/tmpchunk"
+
+rm -rf "$tmpchunk"
+mkdir "$tmpchunk" || exit
+
+k=0
+while read stripes; do
+    echo "============================= $k == $stripes ========================"
+    eval `awk '
+        BEGIN{ i=0; }
+        /^position: /{
+            print "chunkid"   i "=" $4  ";";
+            print "chunkvers" i "=" $6  ";";
+            print "srvhost"   i "=" $10 ";";
+            print "srvport"   i "=" $11 ";";
+            i++;
+        }
+    ' "$fenumout"`
+    rm -rf "$tmpchunk/$k"
+    mkdir "$tmpchunk/$k" || exit
+    m=-1
+    s=0
+    for i in $stripes; do
+        if [ $m -lt 0 -a $s -eq 0 ]; then
+            m=$i
+            s=1
+            continue
+        fi
+        eval srvport='$srvport'$i
+        eval chunksuf='$chunkid'$i'.$chunkvers'$i
+        chunkf=`echo "$qfstestdir/chunk/$srvport/"*/*".$chunksuf"`
+        ls -l "$chunkf"
+        mv "$chunkf" "$tmpchunk/$k" || exit
+    done
+
+    s=0
+    for n in $m ; do
+        [ $m -lt 0 ] && continue
+        [ $s -ne 0 -a $n -eq $m ] && continue
+        s=0
+        eval srvport='$srvport'$n
+        eval srvhost='$srvhost'$n
+        eval chunkid='$chunkid'$n
+        if [ x"$srvport" = x'20400' ]; then
+            srvportr=20401
+        else
+            srvportr=20400
+        fi
+
+        echo "forcing recovery: chunk: $chunkid port: $srvportr"
+        "$toolsdir"/qfsadmin -s "$metahost" -p "$metaport" -f "$clirootcfg" -a \
+            -F "Chunk=$chunkid" \
+            -F "Host=$srvhost" \
+            -F "Port=$srvportr" \
+            -F "Recovery=1" \
+            force_replication || exit
+    done
+
+    while sleep 3; do
+        "$toolsdir"/qfsdataverify \
+            -s "$metahost" -p "$metaport" -f "$clicfg" -c -d -k \
+                "/user/$usr/testrep.dat" 1>/dev/null 2>/dev/null && break
+    done
+    mv "$fenumout" "$fenumout.prev"
+    "$toolsdir"/qfsfileenum -s "$metahost" -p "$metaport" -c "$clicfg" \
+        -f "/user/$usr/testrep.dat" > "$fenumout"
+    cat "$fenumout"
+    sed -e 's/ [0-9]*$//' "$fenumout"      > "$fenumout.np"
+    sed -e 's/ [0-9]*$//' "$fenumout.prev" > "$fenumout.prev.np"
+    diff -du  "$fenumout.prev.np" "$fenumout.np"
+    echo "============================= $k == $stripes ========================"
+    k=`expr $k + 1`
+done << EOF
+    6 3 7 8
+    0 0 1 2
+    5 5 6
+    -1 4 5
+EOF
+# Format:
+# <stripe to force recovery> <stripe to delete> <stripe to delete> <stripe to delete>
