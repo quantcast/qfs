4c08bc7aab81ec0781973b01c319d9977ffeee30
Mike Ovsiannikov <movsiannikov@quantcast.com>
2014-07-28 18:31:36 -0700

    Common library: minimize qfs specific "include" files in kfsdecls.h to fix external "includes" dependencies. Move qfs includes and methods that depend on them into kfsdecls.cc. Remove unused ServerLocation::Distance() method.

---
+++ src/cc/common/kfsdecls.cc
@@ -0,0 +1,73 @@
+//---------------------------------------------------------- -*- Mode: C++ -*-
+// $Id$
+//
+// Created 2014/7/28
+// Author: Mike Ovsiannikov
+//
+// Copyright 2014 Quantcast Corp.
+//
+// This file is part of Kosmos File System (KFS).
+//
+// Licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// \file kfsdecls.cc
+//
+//----------------------------------------------------------------------------
+
+#include "kfsdecls.h"
+
+#include "IntToString.h"
+#include "RequestParser.h"
+
+#include <stdlib.h>
+#include <ctype.h>
+
+namespace KFS
+{
+
+    string
+ServerLocation::ToString() const
+{
+    string ret;
+    ret.reserve(hostname.size() + 16);
+    ret.assign(hostname.data(), hostname.size());
+    ret.append(1, (char)' ');
+    AppendDecIntToString(ret, port);
+    return ret;
+}
+
+    bool
+ServerLocation::FromString(const char* str, size_t len)
+{
+    const char*       ptr = str;
+    const char* const end = ptr + len;
+    while (ptr < end && (*ptr & 0xFF) <= ' ' && *ptr) {
+        ++ptr;
+    }
+    const char* const sptr = ptr;
+    while (ptr < end && (*ptr & 0xFF) > ' ') {
+        ++ptr;
+    }
+    if (ptr <= sptr || *ptr == 0) {
+        Reset(0, -1);
+        return false;
+    }
+    hostname.assign(sptr, ptr - sptr);
+    if (! DecIntParser::Parse(ptr, end - ptr, port) || port < 0) {
+        Reset(0, -1);
+        return false;
+    }
+    return true;
+}
+
+} // namespace KFS
--- src/cc/common/kfsdecls.h
+++ src/cc/common/kfsdecls.h
@@ -30,9 +30,6 @@
 
 #include "kfstypes.h"
 
-#include "IntToString.h"
-#include "RequestParser.h"
-
 #include <ostream>
 #include <istream>
 #include <string>
@@ -49,17 +46,27 @@ using std::min;
 /// Define a server process' location: hostname and the port at which
 /// it is listening for incoming connections
 ///
-struct ServerLocation {
-    ServerLocation(): hostname(), port(-1) {}
-    ServerLocation(const ServerLocation& other):
-        hostname(other.hostname), port(other.port) {}
-    ServerLocation(const string& h, int p): hostname(h), port(p) {}
+struct ServerLocation
+{
+    ServerLocation():
+        hostname(),
+        port(-1)
+        {}
+    ServerLocation(const ServerLocation& other)
+        : hostname(other.hostname),
+          port(other.port)
+        {}
+    ServerLocation(const string& h, int p)
+        : hostname(h),
+          port(p)
+        {}
     ServerLocation& operator = (const ServerLocation& other) {
         hostname = other.hostname;
-        port = other.port;
+        port     = other.port;
         return *this;
     }
-    void Reset(const char* h, int p) {
+    void Reset(const char* h, int p)
+    {
         if (h) {
             hostname = h;
         } else {
@@ -67,91 +74,43 @@ struct ServerLocation {
         }
         port = p;
     }
-    bool operator == (const ServerLocation &other) const {
-        return hostname == other.hostname && port == other.port;
-    }
-    bool operator != (const ServerLocation &other) const {
-        return hostname != other.hostname || port != other.port;
-    }
-    bool operator < (const ServerLocation &other) const {
+    bool operator == (const ServerLocation& other) const
+        { return (hostname == other.hostname && port == other.port); }
+    bool operator != (const ServerLocation& other) const
+        { return (hostname != other.hostname || port != other.port); }
+    bool operator < (const ServerLocation& other) const
+    {
         const int res = hostname.compare(other.hostname);
         return (res < 0 || (res == 0 && port < other.port));
     }
-    bool IsValid() const {
+    bool IsValid() const
+    {
         // Hostname better be non-null and port better
         // be a positive number
         return (! hostname.empty() && port > 0);
     }
-
-    // a proxy for distance between two hosts: take the difference
-    // between their hostnames.  this will mostly work as long as all
-    // machines in the cluster are named as nodeXXX, where XXX is a number
-    int Distance(const string &otherhost) {
-        int len = (int) min(hostname.size(), otherhost.size());
-        int hosta = 0, hostb = 0;
-        int scalefactor = 1;
-
-        for (int i = len - 1; i >= 0; --i) {
-            if (isdigit(hostname[i]))
-                hosta += (hostname[i] - '0') * scalefactor;
-            if (isdigit(otherhost[i]))
-                hostb += (otherhost[i] - '0') * scalefactor;
-            scalefactor *= 10;
-        }
-        return abs(hosta - hostb);
-    }
-    string ToString() const {
-        string ret;
-        ret.reserve(hostname.size() + 16);
-        ret.assign(hostname.data(), hostname.size());
-        ret.append(1, (char)' ');
-        AppendDecIntToString(ret, port);
-        return ret;
-    }
-    ostream& Display(ostream& os) const {
-        return (os << hostname << ' ' << port);
-    }
-    bool FromString(const string& s) {
-        return FromString(s.data(), s.size());
-    }
-    bool FromString(const char* str) {
+    string ToString() const;
+    ostream& Display(ostream& os) const
+        { return (os << hostname << ' ' << port); }
+    bool FromString(const string& s)
+        { return FromString(s.data(), s.size()); }
+    bool FromString(const char* str)
+    {
         const char* const kNull = 0;
-        return FromString(str, (kNull - 1) - str);
+        return FromString(str, str - kNull);
     }
-    bool FromString(const char* str, size_t len) {
-        const char*       ptr = str;
-        const char* const end = ptr + len;
-        while (ptr < end && (*ptr & 0xFF) <= ' ' && *ptr) {
-            ++ptr;
-        }
-        const char* const sptr = ptr;
-        while (ptr < end && (*ptr & 0xFF) > ' ') {
-            ++ptr;
-        }
-        if (ptr <= sptr || *ptr == 0) {
-            Reset(0, -1);
-            return false;
-        }
-        hostname.assign(sptr, ptr - sptr);
-        if (! DecIntParser::Parse(ptr, end - ptr, port) || port < 0) {
-            Reset(0, -1);
-            return false;
-        }
-        return true;
-    }
-
+    bool FromString(const char* str, size_t len);
     string hostname; //!< Location of the server: machine name/IP addr
     int    port;     //!< Location of the server: port to connect to
 };
 
 inline static ostream&
-operator<<(ostream& os, const ServerLocation& loc) {
-    return loc.Display(os);
-}
+operator<<(ostream& os, const ServerLocation& loc)
+    { return loc.Display(os); }
+
 inline static istream&
-operator>>(istream& is, ServerLocation& loc) {
-    return (is >> loc.hostname >> loc.port);
-}
+operator>>(istream& is, ServerLocation& loc)
+    { return (is >> loc.hostname >> loc.port); }
 
 // I-node (file / directory) permissions.
 template<typename UserAndGroupsT>
