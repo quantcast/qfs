6344b4ca754993b98aabe0b38846136ebcac6268
Mike Ovsiannikov <movsiannikov@quantcast.com>
2014-09-21 16:15:21 -0700

    Test scripts: qfs Hadoop test script: create and pass delegation tokens to map / reduce workers via Hadoop's environment variable property. Add option to emit environment variables that need be added to conf/hadoop-env.sh in order to make pseudo distributed mode work with Hadoop 1.x. Add Hadoop 2.x and Yarn experimental code, and notes.

--- src/test-scripts/qfshadoop.sh
+++ src/test-scripts/qfshadoop.sh
@@ -27,16 +27,97 @@
 
 mydir="`dirname "$0"`"
 mydir="`cd "$mydir" && pwd`"
-myqfssrc="`cd "$mydir/../.." && pwd`"
+myqfssrc="`cd "$mydir/../.." > /dev/null && pwd`"
 
 metaserverhost=${metaserverhost-127.0.0.1}
 metaserverport=${metaserverport-20000}
+clientconfig=${clientconfig-"$HOME/qfsbase/client/client.prp"}
 qfssrcprefix=${qfssrcprefix-$myqfssrc}
 hadoopbin=${hadoopbin-bin/hadoop}
 
+showhelp()
+{
+    cat << EOF
+
+Usage: "$0" {fs|jar|-env|-h}
+
+This script is intended to be executed in Hadoop install directory, in place
+    of bin/hadoop with "jar" and "fs" arguments.
+
+The assumption is that the sample servers are already successfully installed,
+    and running.
+
+    QFS servers can be started by the following:
+     ${myqfssrc}/examples/sampleservers/sample_setup.py \
+-r ${myqfssrc}/build/release -a install
+    With sample_setup.py an optional --auth parameter can be used in order to
+    configure and use QFS authentication.
+
+The following can be used to build required QFS executables and jar files:
+    cd ${myqfssrc} && make hadoop-jars
+
+The following envoronment variable can be set prior to this script
+    invocation in order to enbable QFS client debug level:
+    QFS_CLIENT_LOG_LEVEL=DEBUG
+
+-env command line option can be used to emit environment vairlabes that have to
+    be added to \$HADOOP_HOME/conf/hadoop-env.sh
+    [or \$HADOOP_HOME/etc/hadoop/hadoop-env.sh] file in order to
+    make Hadoop work with QFS in pseudo distributed mode.
+    This option should be used only *after* [re]configuring and starting QFS
+    file system with sample_setup.py, and *before* starting haddop job and task
+    trackers with \$HADOOP_HOME/bin/start-mapred.sh
+    [or \$HADOOP_HOME/sbin/start-yarn.sh]
+
+    For pseudo distributed mode to work with Hadoop 1.x, the follwoing
+    properties need to be set in \$HADOOP_HOME/core-site.xml
+
+    <property>
+        <name>fs.default.name</name>
+        <value>qfs://${metaserverhost}:${metaserverport}</value>
+    </property>
+    <property>
+        <name>fs.qfs.impl</name>
+        <value>com.quantcast.qfs.hadoop.QuantcastFileSystem</value>
+    </property>
+
+    For hadoop 2.x the the above properties should be set in the
+    \$HADOOP_HOME/etc/hadoop/core-site.xml and property name "fs.defaultFS"
+    should be used in place of "fs.default.name".
+
+  *** Yarn support is not complete yet*: AM / container succeeds, but the map
+    tasks appears to fail for no obvious reason.
+
+    The following notes are primarily intended for myself, if/when I will have
+    time to experiment with yarn again.
+    Passing envoronment variables, including delegation token, and QFS client
+    trace log level using property "mapred.child.env" does not seem to work
+    with yarn.
+    Setting \$HADOOP_CLASSPATH in is not sufficient to make yarn work.
+    One way around this is to sym link qfs and qfs hadoop jars to
+    \$HADOOP_HOME/share/hadoop/common (\$HADOOP_HOME/lib is not in yarn
+    default).
+    To experiment with yarn the following has to be added to
+    etc/hadoop/core-site.xml
+
+    <property>
+        <name>fs.AbstractFileSystem.qfs.impl</name>
+        <value>com.quantcast.qfs.hadoop.Qfs</value>
+    </property>
+
+-h argument emits this message.
+EOF
+}
+
+if [ x"$1" = x"-h" -o x"$1" = x"-help" -o x"$1" = x"--help" ]; then
+    showhelp
+    exit 0
+fi
+
 mybuildprefix="$qfssrcprefix/build"
 myjbuildprefix="$mybuildprefix/java"
-mycbuildprefix="$mybuildprefix/release/lib"
+mycbuildprefix="$mybuildprefix/release"
+mylibbuildprefix="$mycbuildprefix/lib"
 
 myqfsversion="`sh "$qfssrcprefix/src/cc/common/buildversgit.sh" -v | head -1`"
 
@@ -50,20 +131,13 @@ else
         myhadoop="`which hadoop`"
         if [ x"$myhadoop" = x ]; then
             echo "Error: failed to find hadoop executalbe." 1>&2
-            cat << EOF
-This script is intended to be executed in hadoops install directory, in place
-of bin/hadoop with "jar" and "fs" arguments.
-The assumption is that the sample servers are already successfully installed and
-running, for example using the following:
-     ${myqfssrc}/examples/sampleservers/sample_setup.py \
--r ${myqfssrc}/build/release C-a install
-The following can be used to build required QFS executables and jar files:
-    cd ${myqfssrc} && make hadoop-jars
-EOF
+            showhelp
             exit 1
         fi
     fi
 fi
+myhadoopdir="`dirname "$myhadoop"`"
+myhadoopdir="`cd "$myhadoopdir/.." > /dev/null && pwd`"
 myhadoopvers="`"$myhadoop" version | awk '/^Hadoop /{print $2}'`"
 
 myqfsaccessjar="$myjbuildprefix/qfs-access/qfs-access-${myqfsversion}.jar"
@@ -89,41 +163,190 @@ if [ $mystatus -ne 0 ]; then
     exit $mystatus
 fi
 
+if grep -v '#' "$myhadoop" | grep "JAVA_LIBRARY_PATH=''" > /dev/null; then
+    if [ -f "$myhadoopdir/lib/native/`uname`"*/*qfs_access* ]; then
+        true
+    else
+        cat << EOF
+The follwoing line in $myhadoop script has to be commented out, or qfs native
+libraries, have to be sym linked or copied to the platfrom native libraries
+directory.
+    JAVA_LIBRARY_PATH=''
+EOF
+        if tty -s ; then
+            echo "Would you like to comment out the line?(Y/n)"
+            read ln
+            if [ x"$ln" = x'Y' -o x"$ln" = x'y' -o x"$ln" = x \
+                    -o x"$ln" = x'yes' ]; then
+                mytemp="$myhadoop.$$.tmp"
+                sed -e 's/\(JAVA_LIBRARY_PATH='\'''\''*.$\)/# \1/' \
+                    "$myhadoop" > "$mytemp"         || exit
+                chmod +x "$mytemp"                  || exit
+                mv "$myhadoop" "$myhadoop.qfs.orig" || exit
+                mv "$mytemp"   "$myhadoop"          || exit
+            else
+                exit 1
+            fi
+        else
+            exit 1
+        fi
+    fi
+fi
+
+if [ x"$1" = x'-env' ]; then
+    cat << EOF
+HADOOP_CLASSPATH="${myqfsaccessjar}:${myqfshadoopjar}:\${HADOOP_CLASSPATH}"
+export HADOOP_CLASSPATH
+EOF
+    myosname="`uname`"
+    myqfsaccess="`echo "$myhadoopdir/lib/native/${myosname}"*/*qfs_access*`"
+    if [ -f "$myqfsaccess" ]; then
+        myqfsaccessdir="`dirname "$myqfsaccess"`"
+        cat << EOF
+LD_LIBRARY_PATH="${myqfsaccessdir}:\${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH
+EOF
+    else
+        cat << EOF
+JAVA_LIBRARY_PATH="${mylibbuildprefix}:\${JAVA_LIBRARY_PATH}"
+export JAVA_LIBRARY_PATH
+LD_LIBRARY_PATH="${mylibbuildprefix}:\${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH
+EOF
+    fi
+    if [ -f "$clientconfig" ]; then
+        cat << EOF
+QFS_CLIENT_CONFIG='FILE:${clientconfig}'
+export QFS_CLIENT_CONFIG
+EOF
+    fi
+    exit 0
+fi
+
 HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:${myqfsaccessjar}"
 HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:${myqfshadoopjar}"
 export HADOOP_CLASSPATH
 
 if [ x"$LD_LIBRARY_PATH" = x ]; then
-    JAVA_LIBRARY_PATH="${mycbuildprefix}"
+    JAVA_LIBRARY_PATH="${mylibbuildprefix}"
 else
-    JAVA_LIBRARY_PATH="${JAVA_LIBRARY_PATH}:${mycbuildprefix}"
+    JAVA_LIBRARY_PATH="${mylibbuildprefix}:${JAVA_LIBRARY_PATH}"
 fi
 export JAVA_LIBRARY_PATH
 
 if [ x"$LD_LIBRARY_PATH" = x ]; then
-    LD_LIBRARY_PATH="${mycbuildprefix}"
+    LD_LIBRARY_PATH="${mylibbuildprefix}"
 else
-    LD_LIBRARY_PATH="${mycbuildprefix}:${LD_LIBRARY_PATH}"
+    LD_LIBRARY_PATH="${mylibbuildprefix}:${LD_LIBRARY_PATH}"
 fi
 export LD_LIBRARY_PATH
+
+if [ $# -le 0 ]; then
+    showhelp
+    exit 1
+    # echo ''
+    # exec "$myhadoop"
+fi
 
 mycmd=$1
 shift
 if [ x"$mycmd" = x"jar" ]; then
     myjarname=$1
     shift
-    myprogname=$1
-    shift
+    if [ $# -gt 0 ]; then
+        myprogname=$1
+        shift
+    fi
 else
     unset myjarname
     unset myprogname
 fi
-exec "$myhadoop" \
+
+unset mychildenv
+if [ -f "$clientconfig" ]; then
+    myqfstool="${mycbuildprefix}/bin/tools/qfs"
+    if [ x"$mycmd" = x"jar" -a \
+            \( x"$forcedelegation" = x'yes' -o  -x "${myqfstool}" \) ]; then
+        if [ -x "${myqfstool}" ]; then
+            true
+        else
+            echo "Failed to find executable: ${myqfstool}" 1>&2
+            exit 1
+        fi
+        eval `QFS_CLIENT_CONFIG='' "${myqfstool}" \
+            -cfg "$clientconfig" \
+            -fs  qfs://"${metaserverhost}:${metaserverport}" \
+            -delegate 0 3600000 \
+        | awk '
+            BEGIN {
+                tok="";
+                key="";
+	    }
+            /^Token:/{ tok = $2; }
+            /^Key:/  { key = $2; }
+            END      {
+                if (tok == "" || key == "") {
+                    exit 1;
+                }
+                printf("myqfstoken='\''%s'\'';", tok);
+                printf("myqfskey='\''%s'\'';",   key);
+            }
+        '`
+        if [ x"$myqfstoken" = x -o x"$myqfskey" = x ]; then
+            echo "Failed to create QFS delegation token" 1>&2
+            exit 1
+        fi
+        # Use delimiters different than "=" and "," as those are already used by
+        # hadoop environment variables passing logic.
+        mydelegation="DELIM::|client.auth.psk.keyId:${myqfstoken}"
+        mydelegation="${mydelegation}|client.auth.psk.key:${myqfskey}"
+        myenvqfscliconfig=${mydelegation}
+        # WARNING: passing QFS delegation key as a parameter is not "secure",
+        # as exposes the key via "ps" and such.
+        # It is here for illustration purposes only.
+        mychildenv="QFS_CLIENT_CONFIG=${myenvqfscliconfig}"
+    else
+        myenvqfscliconfig="FILE:${clientconfig}"
+    fi
+fi
+
+if echo "$myhadoopvers" | awk -F . '{exit(2 <= $1 ? 0 : 1);}'; then
+    myfsdefault='fs.defaultFS'
+else
+    myfsdefault='fs.default.name'
+fi
+
+if [ x"$QFS_CLIENT_LOG_LEVEL" = x ]; then
+    true
+else
+    if [ x"$mychildenv" = x ]; then
+        mychildenv="${mychildenv},"
+    fi
+    mychildenv="${mychildenv}QFS_CLIENT_LOG_LEVEL=${QFS_CLIENT_LOG_LEVEL}"
+fi
+
+# QFS_CLIENT_LOG_LEVEL=DEBUG \
+# HADOOP_ROOT_LOGGER="ALL,console" \
+QFS_CLIENT_CONFIG="$myenvqfscliconfig" \
+"$myhadoop" \
     "$mycmd" \
     ${myjarname+"$myjarname"} \
     ${myprogname+"$myprogname"} \
     -Dfs.qfs.impl=com.quantcast.qfs.hadoop.QuantcastFileSystem  \
-    -Dfs.default.name=qfs://"${metaserverhost}:${metaserverport}" \
-    -Dfs.qfs.metaServerHost="${metaserverhost}" \
-    -Dfs.qfs.metaServerPort="${metaserverport}" \
+    -Dfs.AbstractFileSystem.qfs.impl=com.quantcast.qfs.hadoop.Qfs  \
+    -D"${myfsdefault}=qfs://${metaserverhost}:${metaserverport}" \
+    ${mychildenv+"-Dmapred.child.env=${mychildenv}"} \
     ${1+"$@"}
+
+status=$?
+
+if [ x"$myqfstoken" = x -o x"$myqfskey" = x ]; then
+    exit $status
+fi
+
+{ echo "$myqfstoken" ; echo "$myqfskey" ; } | "${myqfstool}" \
+    -cfg "$clientconfig" \
+    -fs  qfs://"${metaserverhost}:${metaserverport}" \
+    -cancel
+
+exit $status
