0fd8b9cafcf016f15232d8d433c9bb75c17aadd8
Mike Ovsiannikov <movsiannikov@quantcast.com>
2015-04-26 16:30:15 -0700

    Meta server: log receiver: implement listener, parameter setting, and record block verification and parsing.

--- src/cc/meta/LogReceiver.cc
+++ src/cc/meta/LogReceiver.cc
@@ -32,6 +32,7 @@
 #include "common/kfstypes.h"
 #include "common/MsgLogger.h"
 #include "common/RequestParser.h"
+#include "common/StBuffer.h"
 
 #include "kfsio/NetManager.h"
 #include "kfsio/NetConnection.h"
@@ -45,19 +46,36 @@
 #include <string>
 #include <algorithm>
 #include <iomanip>
+#include <vector>
 
 #include <time.h>
+#include <errno.h>
 
 namespace KFS
 {
 using std::string;
+using std::vector;
 using std::max;
 using std::hex;
 
 class LogReceiver
 {
+public:
+    LogReceiver();
+    ~LogReceiver();
+    void SetParameters(
+        const char*       inPrefixPtr,
+        const Properties& inParameters);
+    void SetNextLogSeq(
+        seq_t inSeq);
+    int Start(
+        NetManager& inNetManager,
+        QCMutex*    inMutexPtr);
+    void Shutdown();
 private:
     class Impl;
+
+    Impl& mImpl;
 };
 
 class LogReceiver::Impl : public IAcceptorOwner
@@ -69,26 +87,106 @@ public:
 
     Impl()
         : IAcceptorOwner(),
-          mReauthTimeout(20),
+          mReAuthTimeout(20),
           mMaxReadAhead(MAX_RPC_HEADER_LEN),
           mTimeout(60),
           mConnectionCount(0),
+          mMaxConnectionCount(8 << 10),
+          mMutexPtr(0),
+          mIpV6OnlyFlag(false),
+          mListenerAddress(),
           mAcceptorPtr(0),
-          mAuthContext()
-        { List::Init(mConnectionsHeadPtr); }
-    ~Impl()
-        { delete mAcceptorPtr; }
+          mAuthContext(),
+          mNextLogSeq(-1),
+          mDeleteFlag(false),
+          mLines(),
+          mParseBuffer()
+    {
+        List::Init(mConnectionsHeadPtr);
+        mLines.reserve(2 << 10);
+        mParseBuffer.Resize(mParseBuffer.Capacity());
+    }
     virtual KfsCallbackObj* CreateKfsCallbackObj(
         NetConnectionPtr& inConnectionPtr);
     void SetParameters(
         const char*       inPrefixPtr,
         const Properties& inParameters)
     {
+        Properties::String theParamName;
+        if (inPrefixPtr) {
+            theParamName.Append(inPrefixPtr);
+        }
+        const size_t thePrefixLen   = theParamName.GetSize();
+        const bool   kHexFormatFlag = false;
+        mListenerAddress.FromString(inParameters.getValue(
+            theParamName.Truncate(thePrefixLen).Append(
+            "listenOn"), mListenerAddress.ToString()), kHexFormatFlag);
+        mReAuthTimeout = inParameters.getValue(
+            theParamName.Truncate(thePrefixLen).Append(
+            "reAuthTimeout"), mReAuthTimeout);
+        mIpV6OnlyFlag = inParameters.getValue(
+            theParamName.Truncate(thePrefixLen).Append(
+            "ipV6OnlyFlag"), mIpV6OnlyFlag ? 1 : 0) != 0;
+        mMaxReadAhead = max(512, min(64 << 20, inParameters.getValue(
+            theParamName.Truncate(thePrefixLen).Append(
+            "maxReadAhead"), mMaxReadAhead)));
+        mMaxConnectionCount = inParameters.getValue(
+            theParamName.Truncate(thePrefixLen).Append(
+            "maxConnectionCount"), mMaxConnectionCount);
+        mTimeout = inParameters.getValue(
+            theParamName.Truncate(thePrefixLen).Append(
+            "timeout"), mTimeout);
+        mAuthContext.SetParameters(
+            theParamName.Truncate(thePrefixLen).Append("auth.").c_str(),
+            inParameters);
     }
-    int Start();
+    void SetNextLogSeq(
+        seq_t inSeq)
+    {
+        mNextLogSeq = inSeq;
+    }
+    int Start(
+        NetManager& inNetManager,
+        QCMutex*    inMutexPtr)
+    {
+        if (mDeleteFlag) {
+            panic("LogReceiver::Impl::Start delete pending");
+            return -EINVAL;
+        }
+        Shutdown();
+        if (! inNetManager.IsRunning()) {
+            KFS_LOG_STREAM_ERROR <<
+                "net manager shutdown" <<
+            KFS_LOG_EOM;
+            return -EINVAL;
+        }
+        if (! mListenerAddress.IsValid()) {
+            KFS_LOG_STREAM_ERROR <<
+                "invalid listen address: " << mListenerAddress <<
+            KFS_LOG_EOM;
+            return -EINVAL;
+        }
+        const bool kBindOnlyFlag = false;
+        mAcceptorPtr = new Acceptor(
+            inNetManager,
+            mListenerAddress,
+            mIpV6OnlyFlag,
+            this,
+            kBindOnlyFlag
+        );
+        if (! mAcceptorPtr->IsAcceptorStarted()) {
+            delete mAcceptorPtr;
+            mAcceptorPtr = 0;
+            KFS_LOG_STREAM_ERROR <<
+                "failed to start acceptor: " << mListenerAddress <<
+            KFS_LOG_EOM;
+            return -ENOTCONN;
+        }
+        return 0;
+    }
     void Shutdown();
-    int GetReauthTimeout() const
-        { return mReauthTimeout; }
+    int GetReAuthTimeout() const
+        { return mReAuthTimeout; }
     int GetMaxReadAhead() const
         { return mMaxReadAhead; }
     int GetTimeout() const
@@ -99,14 +197,66 @@ public:
         Connection& inConnection);
     void Done(
         Connection& inConnection);
+    seq_t GetNextLogSeq() const
+        { return mNextLogSeq; }
+    void Delete()
+    {
+        Shutdown();
+        mDeleteFlag = true;
+        if (0 < mConnectionCount) {
+            return;
+        }
+        delete this;
+    }
+
+    enum { kMaxBlockHeaderLen  = (int)sizeof(seq_t) * 2 + 1 + 16 };
+    enum { kMinParseBufferSize = kMaxBlockHeaderLen <= MAX_RPC_HEADER_LEN ?
+        MAX_RPC_HEADER_LEN : kMaxBlockHeaderLen };
+
+    typedef vector<int> Lines;
+    Lines& GetTmpLines()
+        { return mLines; }
+    char* GetParseBufferPtr()
+        { return mParseBuffer.GetPtr(); }
+    char* GetParseBufferPtr(
+        int inSize)
+    {
+        if (0 < inSize && mParseBuffer.GetSize() < (size_t)inSize) {
+            mParseBuffer.Clear(); // To prevent copy.
+            mParseBuffer.Resize(
+                (inSize + kMinParseBufferSize - 1) /
+                kMinParseBufferSize * kMinParseBufferSize
+            );
+        }
+        return mParseBuffer.GetPtr();
+    }
+
 private:
-    int         mReauthTimeout;
-    int         mMaxReadAhead;
-    int         mTimeout;
-    int         mConnectionCount;
-    Acceptor*   mAcceptorPtr;
-    AuthContext mAuthContext;
-    Connection* mConnectionsHeadPtr[1];
+    typedef StBufferT<char, kMinParseBufferSize> ParseBuffer;
+
+    int            mReAuthTimeout;
+    int            mMaxReadAhead;
+    int            mTimeout;
+    int            mConnectionCount;
+    int            mMaxConnectionCount;
+    QCMutex*       mMutexPtr;
+    bool           mIpV6OnlyFlag;
+    ServerLocation mListenerAddress;
+    Acceptor*      mAcceptorPtr;
+    AuthContext    mAuthContext;
+    seq_t          mNextLogSeq;
+    bool           mDeleteFlag;
+    Lines          mLines;
+    ParseBuffer    mParseBuffer;
+    Connection*    mConnectionsHeadPtr[1];
+
+    ~Impl()
+    {
+        if (mConnectionCount != 0) {
+            panic("LogReceiver::~Impl: invalid connection count");
+        }
+        delete mAcceptorPtr;
+    }
 };
 
 class LogReceiver::Impl::Connection :
@@ -132,7 +282,7 @@ public:
           mBlockLength(-1),
           mPendingOpsCount(0),
           mBlockChecksum(0),
-          mBlockSeq(-1),
+          mBlockEndSeq(-1),
           mDownFlag(false),
           mAuthPendingResponsesHeadPtr(0),
           mAuthPendingResponsesTailPtr(0),
@@ -265,9 +415,12 @@ public:
         return 0;
     }
 private:
-    enum {
+    enum
+    {
         kAckReauthFlag = 1
     };
+    typedef Impl::Lines Lines;
+
     Impl&                  mImpl;
     string                 mAuthName;
     int64_t                mSessionExpirationTime;
@@ -279,7 +432,7 @@ private:
     int                    mBlockLength;
     int                    mPendingOpsCount;
     int32_t                mBlockChecksum;
-    int64_t                mBlockSeq;
+    int64_t                mBlockEndSeq;
     bool                   mDownFlag;
     MetaRequest*           mAuthPendingResponsesHeadPtr;
     MetaRequest*           mAuthPendingResponsesTailPtr;
@@ -287,7 +440,6 @@ private:
     IOBuffer::WOStream     mOstream;
     Connection*            mPrevPtr[1];
     Connection*            mNextPtr[1];
-    char                   mParseBuffer[MAX_RPC_HEADER_LEN];
 
     friend class QCDLListOp<Connection>;
 
@@ -532,7 +684,7 @@ private:
             }
             int               theLen       = inMsgLen - kSeparatorLen;
             const char* const theHeaderPtr = inBuffer.CopyOutOrGetBufPtr(
-                mParseBuffer, theLen);
+                mImpl.GetParseBufferPtr(), theLen);
             QCRTASSERT(inMsgLen - kSeparatorLen == theLen);
             const char*       thePtr    = theHeaderPtr;
             const char* const theEndPtr = thePtr + inMsgLen - kSeparatorLen;
@@ -551,7 +703,10 @@ private:
         }
         MetaRequest* theReqPtr = 0;
         if (ParseLogRecvCommand(
-                    inBuffer, inMsgLen, &theReqPtr, mParseBuffer) ||
+                    inBuffer,
+                    inMsgLen,
+                    &theReqPtr,
+                    mImpl.GetParseBufferPtr()) ||
                 ! theReqPtr ||
                 META_AUTHENTICATE != theReqPtr->op) {
             MetaRequest::Release(theReqPtr);
@@ -600,20 +755,20 @@ private:
             Error("block checksum mimatch");
             return -1;
         }
-        int         theMaxHdrLen    = (int)sizeof(seq_t) * 2 + 1 + 16;
-        QCASSERT(theMaxHdrLen <= MAX_RPC_HEADER_LEN);
+        int         theMaxHdrLen    =
+            min(mBlockLength, (int)kMaxBlockHeaderLen);
         const char* theStartPtr     = inBuffer.CopyOutOrGetBufPtr(
-                mParseBuffer, theMaxHdrLen);
+                mImpl.GetParseBufferPtr(), theMaxHdrLen);
         const char* const theEndPtr = theStartPtr + theMaxHdrLen;
-        int64_t     theBlockSeq = -1;
-        const char* thePtr      = theStartPtr;
+        int64_t     theBlockEndSeq  = -1;
+        const char* thePtr          = theStartPtr;
         if (! HexIntParser::Parse(
-                thePtr, theEndPtr - thePtr, theBlockSeq) ||
-                theBlockSeq < 0 ||
-                (0 <= mBlockSeq && theBlockSeq != mBlockSeq + 1)) {
+                thePtr, theEndPtr - thePtr, theBlockEndSeq) ||
+                theBlockEndSeq < 0 ||
+                (0 <= mBlockEndSeq && theBlockEndSeq < mBlockEndSeq)) {
             KFS_LOG_STREAM_ERROR << GetPeerName() <<
-                " invalid block sequence: " << theBlockSeq <<
-                " last    : "               << mBlockSeq <<
+                " invalid block sequence: " << theBlockEndSeq <<
+                " last    : "               << mBlockEndSeq <<
                 " length: "                 << mBlockLength <<
             KFS_LOG_EOM;
             MsgLogLines(MsgLogger::kLogLevelERROR,
@@ -626,7 +781,7 @@ private:
         }
         if (theEndPtr <= thePtr && theMaxHdrLen < mBlockLength) {
             KFS_LOG_STREAM_ERROR << GetPeerName() <<
-                " invalid block header:" << theBlockSeq  <<
+                " invalid block header:" << theBlockEndSeq  <<
                 " length: "              << mBlockLength <<
             KFS_LOG_EOM;
             MsgLogLines(MsgLogger::kLogLevelERROR,
@@ -634,8 +789,8 @@ private:
             Error("invalid block header");
             return -1;
         }
-        inBuffer.Consume((int)(thePtr - theStartPtr));
-        mBlockSeq = theBlockSeq;
+        mBlockLength -= inBuffer.Consume((int)(thePtr - theStartPtr));
+        mBlockEndSeq = theBlockEndSeq;
         SendAck();
         if (mDownFlag) {
             return -1;
@@ -651,8 +806,8 @@ private:
         KFS_LOG_STREAM_ERROR << GetPeerName() <<
             " error:" << (inMsgPtr ? inMsgPtr : "")  <<
             " closing connection"
-            " last block: "   << mBlockSeq <<
-            " socket error: " << mConnectionPtr->GetErrorMsg() <<
+            " last block end: " << mBlockEndSeq <<
+            " socket error: "   << mConnectionPtr->GetErrorMsg() <<
         KFS_LOG_EOM;
         mConnectionPtr->Close();
         mDownFlag = true;
@@ -666,7 +821,7 @@ private:
         if (GetAuthContext().IsAuthRequired()) {
             uint64_t const theUpdateCount = GetAuthContext().GetUpdateCount();
             theReAuthFlag = theUpdateCount != mAuthCtxUpdateCount ||
-                mSessionExpirationTime < TimeNow() + mImpl.GetReauthTimeout();
+                mSessionExpirationTime < TimeNow() + mImpl.GetReAuthTimeout();
             if (theReAuthFlag) {
                 KFS_LOG_STREAM_INFO << GetPeerName() <<
                     " requesting re-authentication:"
@@ -684,7 +839,7 @@ private:
         }
         ReqOstream theStream(mOstream.Set(mConnectionPtr->GetOutBuffer()));
         theStream << hex <<
-            "A: " << mBlockSeq << " " << theAckFlags << "\n";
+            "A: " << mBlockEndSeq << " " << theAckFlags << "\n";
         theStream.flush();
         if (mRecursionCount <= 0) {
             mConnectionPtr->StartFlush();
@@ -694,9 +849,84 @@ private:
         IOBuffer& inBuffer)
     {
         if (mBlockLength < 0) {
+            Error("invalid negative block length");
             return -1;
         }
-        inBuffer.Consume(mBlockLength);
+        if (0 < mBlockLength && mBlockEndSeq <= mImpl.GetNextLogSeq()) {
+            Lines& theLines = mImpl.GetTmpLines();
+            theLines.clear();
+            int  theRem        = mBlockLength;
+            bool theAppendFlag = false;
+            for (IOBuffer::iterator theIt = inBuffer.begin();
+                    theRem <= 0 && theIt != inBuffer.end();
+                    ++theIt) {
+                const char* const theStartPtr = theIt->Producer();
+                const char* const theEndPtr   =
+                    min(theIt->Consumer(), theStartPtr + theRem);
+                if (theEndPtr <= theStartPtr) {
+                    continue;
+                }
+                theRem -= theEndPtr - theStartPtr;
+                const char* thePtr = theEndPtr;
+                const char* theNPtr;
+                while (thePtr < theEndPtr &&
+                        (theNPtr = reinterpret_cast<const char*>(
+                            memchr(thePtr, '\n', theEndPtr - thePtr)))) {
+                    ++theNPtr;
+                    const int theLen = (int)(theNPtr - thePtr);
+                    if (theAppendFlag) {
+                        theAppendFlag = false;
+                        theLines.back() += theLen;
+                    } else {
+                        theLines.push_back(theLen);
+                    }
+                    thePtr = theNPtr;
+                }
+                if (thePtr < theEndPtr) {
+                    const int theLen = (int)(theNPtr - thePtr);
+                    if (theAppendFlag) {
+                        theLines.back() += theLen;
+                    } else {
+                        theLines.push_back(theLen);
+                    }
+                    theAppendFlag = true;
+                }
+            }
+            if (theRem != 0) {
+                panic("LogReceiver::Impl::Connection::ProcessBlock:"
+                    " internal error");
+                return -1;
+            }
+            if (theAppendFlag) {
+                const char* theMsgPtr =
+                    "invalid log block format: no trailing new line";
+                KFS_LOG_STREAM_ERROR <<
+                    theMsgPtr <<
+                    " lines: " << theLines.size() <<
+                KFS_LOG_EOM;
+                Error(theMsgPtr);
+                return -1;
+            }
+            for (Lines::const_iterator theIt = theLines.begin();
+                    theIt != theLines.end();
+                    ++theIt) {
+                const int theLen = *theIt;
+                if (theLen <= 0) {
+                    panic("LogReceiver::Impl: invalid line length");
+                    continue;
+                }
+                int               theLLen = theLen;
+                const char* const thePtr  = inBuffer.CopyOutOrGetBufPtr(
+                    mImpl.GetParseBufferPtr(theLen), theLLen);
+                if (theLLen != theLen || (thePtr[theLen - 1] & 0xFF) != '\n') {
+                    panic("LogReceiver::Impl: invalid copy length");
+                }
+                // Replay log line here.
+                inBuffer.Consume(theLen);
+            }
+        } else {
+            inBuffer.Consume(mBlockLength);
+        }
         mConnectionPtr->SetMaxReadAhead(mImpl.GetMaxReadAhead());
         return 0;
     }
@@ -744,10 +974,19 @@ private:
 LogReceiver::Impl::CreateKfsCallbackObj(
     NetConnectionPtr& inConnectionPtr)
 {
-    if (inConnectionPtr && inConnectionPtr->IsGood()) {
-        return new Connection(*this, inConnectionPtr);
+    if (! inConnectionPtr || ! inConnectionPtr->IsGood()) {
+        return 0;
     }
-    return 0;
+    if (mMaxConnectionCount <= mConnectionCount) {
+        KFS_LOG_STREAM_ERROR <<
+            "log receiver: reached connections limit"
+            " of: "                 << mMaxConnectionCount <<
+            " connections: "        << mConnectionCount <<
+            " closing connection: " << inConnectionPtr->GetPeerName() <<
+        KFS_LOG_EOM;
+        return 0;
+    }
+    return new Connection(*this, inConnectionPtr);
 }
 
     void
@@ -770,16 +1009,53 @@ LogReceiver::Impl::Done(
     }
     List::Remove(mConnectionsHeadPtr, inConnection);
     mConnectionCount--;
+    if (mDeleteFlag && mConnectionCount <= 0) {
+        delete this;
+    }
 }
 
     void
 LogReceiver::Impl::Shutdown()
 {
+    delete mAcceptorPtr;
+    mAcceptorPtr = 0;
     List::Iterator theIt(mConnectionsHeadPtr);
     Connection* thePtr;
     while (0 < mConnectionCount && (thePtr = theIt.Next())) {
         thePtr->HandleEvent(EVENT_NET_ERROR, 0);
     }
+}
+
+LogReceiver::LogReceiver()
+    : mImpl(*(new Impl()))
+{
+}
+
+LogReceiver::~LogReceiver()
+{
+    mImpl.Delete();
+}
+
+    void
+LogReceiver::SetParameters(
+    const char*       inPrefixPtr,
+    const Properties& inParameters)
+{
+    mImpl.SetParameters(inPrefixPtr, inParameters);
+}
+
+    int
+LogReceiver::Start(
+    NetManager& inNetManager,
+    QCMutex*    inMutexPtr)
+{
+    return mImpl.Start(inNetManager, inMutexPtr);
+}
+
+    void
+LogReceiver::Shutdown()
+{
+    mImpl.Shutdown();
 }
 
 } // namespace KFS
