52c31c1f0ee29fc230505ec5ed60defdac19bf0f
Mike Ovsiannikov <movsiannikov@quantcast.com>
2015-11-22 02:49:47 -0800

    Meta server: fix code formatting.

--- src/cc/meta/LayoutManager.cc
+++ src/cc/meta/LayoutManager.cc
@@ -10965,9 +10965,9 @@ LayoutManager::RebalanceCtrs::Show(
     "PlanCannotMove"                << pref << mPlanCannotMove << suf <<
     "PlanReplicationStarted"        << pref << mPlanReplicationStarted << suf <<
     "PlanNoReplicationStarted"      << pref << mPlanNoReplicationStarted << suf <<
-    "PlanLine"              << pref << mPlanLine << suf <<
-    "PlanNoServer"              << pref << mPlanNoServer << suf <<
-    "PlanAdded"             << pref << mPlanAdded << suf <<
+    "PlanLine"                      << pref << mPlanLine << suf <<
+    "PlanNoServer"                  << pref << mPlanNoServer << suf <<
+    "PlanAdded"                     << pref << mPlanAdded << suf <<
     "TotalPlanNoDest"               << pref << mTotalPlanNoDest << suf <<
     "TotalPlanTimeout"              << pref << mTotalPlanTimeout << suf <<
     "TotalPlanScanned"              << pref << mTotalPlanScanned << suf <<
