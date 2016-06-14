0d860e5a731412f011041a3f98f6c4b67f81f100
Mike Ovsiannikov <movsiannikov@quantcast.com>
2016-06-14 08:50:40 +0200

    Client library: remove extraneous object store file seek restriction at the kfs client level, in order to allow to set position (seek) with object store files. Object store block write path now enforces sequential write with object store files.

--- src/cc/libclient/KfsClient.cc
+++ src/cc/libclient/KfsClient.cc
@@ -4545,10 +4545,6 @@ KfsClientImpl::Seek(int fd, chunkOff_t offset, int whence)
     if (newOff < 0) {
         return -EINVAL;
     }
-    if (0 == entry.fattr.numReplicas &&
-            entry.currPos.fileOffset != newOff) {
-        return -ESPIPE;
-    }
     entry.currPos.fileOffset = newOff;
 
     return newOff;
