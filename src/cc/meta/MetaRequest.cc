a8b856ce28cf723053d3ea1ee150da5c7a11fd42
Mike Ovsiannikov <movsiannikov@quantcast.com>
2016-12-20 18:48:48 -0800

    Meta server: fix sporadic file descriptor close in checkpoint write with lock file by invalidating lock file descriptor after close.

--- src/cc/meta/MetaRequest.cc
+++ src/cc/meta/MetaRequest.cc
@@ -3355,6 +3355,7 @@ MetaCheckpoint::handle()
         }
         if (lockFd >= 0) {
             close(lockFd);
+            lockFd = -1;
         }
         if (failedCount > maxFailedCount) {
             panic("checkpoint failures", false);
@@ -3383,6 +3384,7 @@ MetaCheckpoint::handle()
     }
     if (lockFd >= 0) {
         close(lockFd);
+        lockFd = -1;
     }
     if (! lockFileName.empty() &&
             (lockFd = try_to_acquire_lockfile(lockFileName)) < 0) {
@@ -3399,6 +3401,7 @@ MetaCheckpoint::handle()
         KFS_LOG_EOM;
         if (lockFd >= 0) {
             close(lockFd);
+            lockFd = -1;
         }
         return;
     }
@@ -3408,6 +3411,7 @@ MetaCheckpoint::handle()
     if (cp.isCPNeeded()) {
         if (lockFd >= 0) {
             close(lockFd);
+            lockFd = -1;
         }
         if (lastCheckpointId != oplog.checkpointed()) {
             panic("finish log failure");
