6bafff1aadbd7827543e46fd5513c94e41b87766
Mike Ovsiannikov <movsiannikov@quantcast.com>
2017-01-16 04:02:13 -0800

    Travis: enable -Werror and rat on linux; do not use sudo if already root, as sudo might not be installed yet, add ubuntu 16.04 build.

--- travis/script.sh
+++ travis/script.sh
@@ -31,34 +31,36 @@ MVN_TAR="apache-maven-3.0.5-bin.tar.gz"
 MVN_URL="http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.0.5/binaries/$MVN_TAR"
 TAIL_TEST_LOGS='{ find build/release/qfstest -type f -name \*.log -print0 | xargs -0  tail -n 500 ; exit 1; }'
 
+MYCMAKE_OPTIONS='-D CMAKE_BUILD_TYPE=RelWithDebInfo'
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     MYOPENSSL_DIR='/usr/local/Cellar/openssl/'
     MYOPENSSL_R_DIR=$(ls -1 "$MYOPENSSL_DIR" | tail -n 1)
-    MYCMAKE_OPTIONS="-D OPENSSL_ROOT_DIR=${MYOPENSSL_DIR}${MYOPENSSL_R_DIR}"
-    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D CMAKE_BUILD_TYPE=RelWithDebInfo"
+    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D OPENSSL_ROOT_DIR=${MYOPENSSL_DIR}${MYOPENSSL_R_DIR}"
     sysctl machdep.cpu
     df -h
     make -j 2 CMAKE_OPTIONS="$MYCMAKE_OPTIONS" test tarball || $TAIL_TEST_LOGS
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D QFS_EXTRA_CXX_OPTIONS=-Werror"
+    CMD='if [ x"$(id -u)" = x0 ]; then MYSUDO=; else MYSUDO=sudo; fi; '
     if [[ "$DISTRO" == "ubuntu" ]]; then
-        CMD="sudo apt-get update"
-        CMD="$CMD && sudo apt-get install -y $DEPS_UBUNTU"
+        CMD="$CMD $MYSUDO apt-get update"
+        CMD="$CMD && $MYSUDO apt-get install -y $DEPS_UBUNTU"
 
         # coverage enabled only generated on ubuntu
-        CMD="$CMD && { cat /proc/cpuinfo ; df -h ; true ; }"
-        CMD="$CMD && make -j 2 CMAKE_OPTIONS='-D ENABLE_COVERAGE=yes -D CMAKE_BUILD_TYPE=RelWithDebInfo' test tarball"
+        MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D ENABLE_COVERAGE=ON"
     elif [[ "$DISTRO" == "centos" ]]; then
         CMD="yum install -y $DEPS_CENTOS"
 
         # CentOS doesn't package maven directly so we have to install it manually
         CMD="$CMD && wget $MVN_URL"
-        CMD="$CMD && sudo tar -xf $MVN_TAR -C /usr/local"
+        CMD="$CMD && $MYSUDO tar -xf $MVN_TAR -C /usr/local"
 
         # Set up PATH and links
         CMD="$CMD && pushd /usr/local"
-        CMD="$CMD && sudo ln -s ${MVN_TAR%-bin.tar.gz} maven"
+        CMD="$CMD && $MYSUDO ln -s ${MVN_TAR%-bin.tar.gz} maven"
         CMD="$CMD && export M2_HOME=/usr/local/maven"
         CMD="$CMD && export PATH=\${M2_HOME}/bin:\${PATH}"
         CMD="$CMD && popd"
@@ -67,11 +69,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	    # CentOS7 has the distro information in /etc/redhat-release
 	    CMD="$CMD && cut /etc/redhat-release -d' ' --fields=1,3,4 > /etc/issue"
 	fi
-
-        # now the actual command
-        CMD="$CMD && { cat /proc/cpuinfo ; df -h ; true ; }"
-        CMD="$CMD && make -j 2 test tarball"
     fi
+    CMD="$CMD && { cat /proc/cpuinfo ; df -h ; true ; }"
+    CMD="$CMD && make -j 2 CMAKE_OPTIONS='$MYCMAKE_OPTIONS' rat test tarball"
     CMD="$CMD || $TAIL_TEST_LOGS"
 
     docker run --rm -t -v $PWD:$PWD -w $PWD $DISTRO:$VER /bin/bash -c "$CMD"
