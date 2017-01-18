858d3eb0e70291a6d2c58afffe6b8f77e0f62d3b
Mike Ovsiannikov <movsiannikov@quantcast.com>
2017-01-17 23:37:47 -0800

    Travis: rework build script to make it more readable / maintainable by running the script in docker instead of attempting to stuff the script into single bash command line.

--- travis/script.sh
+++ travis/script.sh
@@ -35,89 +35,132 @@ DEPS_CENTOS=$DEPS_CENTOS' libuuid-devel wget unzip sudo which'
 MVN_TAR="apache-maven-3.0.5-bin.tar.gz"
 MVN_URL="http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.0.5/binaries/$MVN_TAR"
 
-QFS_TEST_DIR='build/release/qfstest'
-TAIL_TEST_LOGS="{ [ -d $QFS_TEST_DIR ] && find $QFS_TEST_DIR"
-TAIL_TEST_LOGS=$TAIL_TEST_LOGS' -type f -name \*.log -print0'
-TAIL_TEST_LOGS=$TAIL_TEST_LOGS' | xargs -0  tail -n 500 ; exit 1; }'
+MYTMPDIR='.tmp'
+MYCODECOV="$MYTMPDIR/codecov.sh"
 
 MYCMAKE_OPTIONS='-D CMAKE_BUILD_TYPE=RelWithDebInfo'
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    MYOPENSSL_DIR='/usr/local/Cellar/openssl/'
-    MYOPENSSL_R_DIR=$(ls -1 "$MYOPENSSL_DIR" | tail -n 1)
-    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D OPENSSL_ROOT_DIR=${MYOPENSSL_DIR}${MYOPENSSL_R_DIR}"
-    sysctl machdep.cpu
-    df -h
-    make -j 2 CMAKE_OPTIONS="$MYCMAKE_OPTIONS" test tarball \
-        || eval $TAIL_TEST_LOGS
-    exit 0
-fi
-
-setsusudo()
+set_sudo()
 {
-    CMD='if [ x"$(id -u)" = x0 ];'
-    CMD="$CMD then MYSUDO=; MYSU='$MYSU'; MYUSER='$MYUSER';"
-    CMD="$CMD else MYSUDO=sudo; MYSU=; MYUSER=; fi;"
+    if [ x"$(id -u)" = x0 ]; then
+        MYSUDO=
+        if [ $# -gt 0 ]; then
+            MYUSER=$1
+        fi
+        if [ x"$MYUSER" = x ]; then
+            MYSU=
+        else
+            MYSU="sudo -u $MYUSER"
+        fi
+    else
+        MYSUDO='sudo'
+        MYSU=
+        MYUSER=
+    fi
 }
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D QFS_EXTRA_CXX_OPTIONS=-Werror"
-    MYUSER=
-    MYSU=
-    CODECOV=
-    if [[ "$DISTRO" == "ubuntu" ]]; then
-        # build and test under qfsbuild user.
-        MYUSER='qfsbuild'
-        MYSU="sudo -u $MYUSER"
-        setsusudo
-        CMD="$CMD \$MYSUDO apt-get update"
-        CMD="$CMD && \$MYSUDO apt-get install -y $DEPS_UBUNTU"
-        CMD=$CMD' && { [ x"$MYUSER" = x ] || {'
-        CMD=$CMD' { id -u $MYUSER 2>/dev/null || useradd -m $MYUSER; }'
-        CMD=$CMD' && chown -R $MYUSER .; } }'
-        # coverage enabled only generated on ubuntu
-        MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D ENABLE_COVERAGE=ON"
-        # run code coverage in docker, don't fail build if it fails
-        # pass travis env vars to code coverage
-        MYTMP=.tmp
-        mkdir -p  "$MYTMP"
-        CODECOV="$MYTMP/codecov.sh"
-        {
-            env | grep -E '^(TRAVIS|CI)' | sed \
-                -e "s/'/'\\\''/g"  \
-                -e "s/=/=\'/" \
-                -e 's/$/'"'/" \
-                -e 's/^/export /' 
-            echo 'curl -s https://codecov.io/bash | /bin/bash'
-            echo 'exit 0'
-        } > "$CODECOV"
-        CODECOV=" && \$MYSU /bin/bash $CODECOV"
-    elif [[ "$DISTRO" == "centos" ]]; then
-        setsusudo
-        CMD="$CMD \$MYSUDO yum install -y $DEPS_CENTOS"
-
-        # CentOS doesn't package maven directly so we have to install it manually
-        CMD="$CMD && wget $MVN_URL"
-        CMD="$CMD && \$MYSUDO tar -xf $MVN_TAR -C /usr/local"
-
-        # Set up PATH and links
-        CMD="$CMD && pushd /usr/local"
-        CMD="$CMD && \$MYSUDO ln -s ${MVN_TAR%-bin.tar.gz} maven"
-        CMD="$CMD && export M2_HOME=/usr/local/maven"
-        CMD="$CMD && export PATH=\${M2_HOME}/bin:\${PATH}"
-        CMD="$CMD && popd"
-
-	if [[ "$VER" == "7" ]]; then
-	    # CentOS7 has the distro information in /etc/redhat-release
-	    CMD="$CMD && cut /etc/redhat-release -d' ' --fields=1,3,4 > /etc/issue"
-	fi
+tail_logs_and_exit()
+{
+    if [ -d "$QFS_TEST_DIR" ]; then
+        find "$QFS_TEST_DIR" -type f -name '*.log' -print0 \
+            | xargs -0  tail -n 500
     fi
-    CMD="$CMD && { cat /proc/cpuinfo ; df -h ; true ; }"
-    CMD="$CMD && \$MYSU make rat clean"
-    CMD="$CMD && \$MYSU make -j 2 CMAKE_OPTIONS='$MYCMAKE_OPTIONS' test tarball"
-    CMD="$CMD $CODECOV || $TAIL_TEST_LOGS"
+    exit 1
+}
 
-    docker run --rm -t -v $PWD:$PWD -w $PWD $DISTRO:$VER /bin/bash -c "$CMD"
+do_build()
+{
+    $MYSU make -j ${1-2} CMAKE_OPTIONS="$MYCMAKE_OPTIONS" test tarball \
+    || tail_logs_and_exit
+}
+
+do_build_linux()
+{
+    if [ -r /proc/cpuinfo ]; then
+        cat /proc/cpuinfo
+    fi
+    df -h || true
+    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D QFS_EXTRA_CXX_OPTIONS=-Werror"
+    $MYSU make rat clean && do_build
+}
+
+init_codecov()
+{
+    # run code coverage in docker
+    # pass travis env vars to code coverage
+    mkdir -p  "$MYTMPDIR"
+    {
+        env | grep -E '^(TRAVIS|CI)' | sed \
+            -e "s/'/'\\\''/g"  \
+            -e "s/=/=\'/" \
+            -e 's/$/'"'/" \
+            -e 's/^/export /'
+        echo 'curl -s https://codecov.io/bash | /bin/bash'
+    } > "$MYCODECOV"
+}
+
+build_ubuntu()
+{
+    # build and test under qfsbuild user.
+    set_sudo 'qfsbuild'
+    $MYSUDO apt-get update
+    $MYSUDO apt-get install -y $DEPS_UBUNTU
+    if [ x"$MYUSER" = x ]; then
+        true
+    else
+        # create regular user to run the build and test under it
+        if id -u "$MYUSER" 2>/dev/null; then
+            useradd -m "$MYUSER"
+        fi
+        chown -R "$MYUSER"
+    fi
+    # coverage enabled only generated on ubuntu
+    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D ENABLE_COVERAGE=ON"
+    do_build_linux
+    /bin/bash "$MYCODECOV" || true
+}
+
+build_centos()
+{
+    set_sudo ''
+    $MYSUDO yum install -y $DEPS_CENTOS
+    # CentOS doesn't package maven directly so we have to install it manually
+    wget "$MVN_URL"
+    $MYSUDO tar -xf "$MVN_TAR" -C /usr/local
+    # Set up PATH and links
+    pushd /usr/local
+    $MYSUDO ln -s ${MVN_TAR%-bin.tar.gz} maven
+    export M2_HOME=/usr/local/maven
+    export PATH=${M2_HOME}/bin:${PATH}
+    popd
+    rm "$MVN_TAR"
+    if [ x"$1" == x'7' ]; then
+        # CentOS7 has the distro information in /etc/redhat-release
+        $MYSUDO /bin/bash -c \
+            "cut /etc/redhat-release -d' ' --fields=1,3,4 > /etc/issue"
+    fi
+    do_build_linux
+}
+
+if [ $# -eq 3 -a x"$1" = x'build' ]; then
+    $1_$2 "$3"
+    exit
 fi
 
-# vim: set tw=0:
+if [ x"$TRAVIS_OS_NAME" = x"linux" ]; then
+    if [ x"$DISTRO" == x"ubuntu" ]; then
+        init_codecov
+    fi
+    docker run --rm -t -v "$PWD:$PWD" -w "$PWD" "$DISTRO:$VER" \
+        /bin/bash ./travis/script.sh build "$DISTRO" "$VER"
+elif [ x"$TRAVIS_OS_NAME" = x"osx" ]; then
+    MYSSLDIR='/usr/local/Cellar/openssl/'
+    MYRELDIR=$(ls -1 "$MYSSLDIR" | tail -n 1)
+    MYCMAKE_OPTIONS="$MYCMAKE_OPTIONS -D OPENSSL_ROOT_DIR=${MYSSLDIR}${MYRELDIR}"
+    sysctl machdep.cpu || true
+    df -h || true
+    do_build
+else
+    echo "OS: $TRAVIS_OS_NAME not yet supported"
+    exit 1
+fi
