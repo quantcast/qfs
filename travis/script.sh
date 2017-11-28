f214f3da8ad21cad830446418bab3d3a77d5691d
Mike Ovsiannikov <movsiannikov@quantcast.com>
2017-11-27 19:57:39 -0800

    Travis: install centos and ubuntu fuse packages, to allow to run fuse test.

--- travis/script.sh
+++ travis/script.sh
@@ -26,11 +26,11 @@ set -ex
 
 DEPS_UBUNTU='g++ cmake git libboost-regex-dev libkrb5-dev libssl-dev python-dev'
 DEPS_UBUNTU=$DEPS_UBUNTU' libfuse-dev default-jdk unzip maven sudo passwd'
-DEPS_UBUNTU=$DEPS_UBUNTU' curl openssl gdb'
+DEPS_UBUNTU=$DEPS_UBUNTU' curl openssl fuse gdb'
 
 DEPS_CENTOS='gcc-c++ make git boost-devel krb5-devel'
 DEPS_CENTOS=$DEPS_CENTOS' python-devel fuse-devel java-openjdk java-devel'
-DEPS_CENTOS=$DEPS_CENTOS' libuuid-devel curl unzip sudo which openssl gdb'
+DEPS_CENTOS=$DEPS_CENTOS' libuuid-devel curl unzip sudo which openssl fuse gdb'
 
 DEPS_CENTOS5=$DEPS_CENTOS' cmake28 openssl101e openssl101e-devel'
 DEPS_CENTOS=$DEPS_CENTOS' openssl-devel cmake'
