8fa5ec4203f37b3eadc71f6960df98a3b1c10239
Mike Ovsiannikov <movsiannikov@quantcast.com>
2018-08-19 20:18:31 -0700

    Common library: update no git version to 2.0.1.

--- src/cc/common/buildversgit.sh
+++ src/cc/common/buildversgit.sh
@@ -19,7 +19,7 @@
 # permissions and limitations under the License.
 
 # default version to use if git is not available
-qfs_no_git_version="2.0.0"
+qfs_no_git_version="2.0.1"
 
 usage() {
     echo "
