bb6342916b94a0d36c9c7c2d712cee2311fe44de
Mike Ovsiannikov <movsiannikov@quantcast.com>
2020-02-27 03:55:17 -0800

    Ext DNS: possible fix for GCC 9 build error.

--- ext/dns/src/dns.h
+++ ext/dns/src/dns.h
@@ -157,7 +157,7 @@ DNS_PUBLIC int *dns_debug_p(void);
 
 #define dns_quietinit(...) \
 	DNS_PRAGMA_PUSH DNS_PRAGMA_QUIET __VA_ARGS__ DNS_PRAGMA_POP
-#elif (__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || __GNUC__ > 4
+#elif (__GNUC__ == 4 && __GNUC_MINOR__ >= 6) || (__GNUC__ > 4 && __GNUC__ < 9)
 #define DNS_PRAGMA_PUSH _Pragma("GCC diagnostic push")
 #define DNS_PRAGMA_QUIET _Pragma("GCC diagnostic ignored \"-Woverride-init\"")
 #define DNS_PRAGMA_POP _Pragma("GCC diagnostic pop")
