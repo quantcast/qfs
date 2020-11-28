75ee32fd80c40b096ec450c5bd90cf649701daaf
Mike Ovsiannikov <movsiannikov@quantcast.com>
2020-11-27 18:11:09 -0800

    Chunk server: fix last change by moving setting node and rack ids before setting thread id of chunk recovery meta server protocol state machine, as later turns on debug instrumentation that prevents state machine modification from any other thread, including the current one.

--- src/cc/chunk/Replicator.cc
+++ src/cc/chunk/Replicator.cc
@@ -1580,6 +1580,8 @@ private:
             );
             ret[i].mMeta->SetMaxMetaLogWriteRetryCount(
                 ret[i].mMeta->GetMaxRetryCount());
+            ret[i].mMeta->SetRackId(gMetaServerSM.GetRackId());
+            ret[i].mMeta->SetNodeId(gMetaServerSM.GetNodeId().c_str());
             if (! authFlag) {
                 ret[i].mMeta->SetCommonRpcHeaders(
                     commonRcpHdrs, commonShortRpcHdrs);
@@ -1587,8 +1589,6 @@ private:
             if (thread && sDebugSetThreadFlag) {
                 ret[i].mMeta->SetThread(&thread->GetThread());
             }
-            ret[i].mMeta->SetRackId(gMetaServerSM.GetRackId());
-            ret[i].mMeta->SetNodeId(gMetaServerSM.GetNodeId().c_str());
             ret[i].mDebugSetThreadUpdateCount = sDebugSetThreadUpdateCount;
         }
         return ret;
