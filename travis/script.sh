197f8613f7aa6a67ddb9947b74060b0d0b1293be
Mike Ovsiannikov <movsiannikov@quantcast.com>
2021-03-29 20:52:13 -0700

    Set env. var with bash line.

--- travis/script.sh
+++ travis/script.sh
@@ -185,7 +185,8 @@ install_maven()
 build_ubuntu()
 {
     $MYSUDO apt-get update
-    $MYSUDO DEBIAN_FRONTEND="noninteractive" apt-get install -y $DEPS_UBUNTU
+    $MYSUDO /bin/bash -c \
+        "DEBIAN_FRONTEND='noninteractive' apt-get install -y $DEPS_UBUNTU"
     if [ x"$1" = x'18.04' ]; then
         QFSHADOOP_VERSIONS=$MYQFSHADOOP_VERSIONS_UBUNTU1804
     fi
