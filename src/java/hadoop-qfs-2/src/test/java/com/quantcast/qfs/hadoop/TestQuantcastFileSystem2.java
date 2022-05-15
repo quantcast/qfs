596cdea288a216d130b19d502a39f154ebcf52c5
Michael Kamprath <michael@kamprath.net>
2022-05-14 21:24:17 -0700

    Support for Hadoop getScheme() (#256)
    * added support for org.apache.hadoop.fs.FileSystem.getScheme()
    
    * corrected location of getScheme() method
    
    * addressing code review comments on code duplication

---
+++ src/java/hadoop-qfs-2/src/test/java/com/quantcast/qfs/hadoop/TestQuantcastFileSystem2.java
@@ -0,0 +1,59 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Unit tests for testing the KosmosFileSystem API implementation.
+ */
+
+package com.quantcast.qfs.hadoop;
+
+import java.io.*;
+import java.net.*;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.fs.Path;
+
+import com.quantcast.qfs.hadoop.QuantcastFileSystem;
+
+
+public class TestQuantcastFileSystem2 extends TestQuantcastFileSystem {
+
+  @Override
+  protected void setUp() throws IOException {
+    Configuration conf = new Configuration();
+
+    qfsEmul = new QFSEmulationImpl(conf);
+    quantcastFileSystem = new QuantcastFileSystem2(qfsEmul, null);
+    // a dummy URI; we are not connecting to any setup here
+    quantcastFileSystem.initialize(URI.create("qfs:///"), conf);
+    baseDir = new Path(System.getProperty("test.build.data", "/tmp" ) +
+                                          "/qfs-test");
+  }
+
+
+  // @Test
+  // Enasure HDFS compatibility
+  public void testHDFSCompatibility() throws Exception {
+
+    assertEquals(quantcastFileSystem.getScheme(), "qfs");
+  }
+}
