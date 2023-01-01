7d6b197cd4c742af52ffe9413419abcb0f31272e
Mike Ovsiannikov <movsiannikov@quantcast.com>
2022-12-31 20:57:26 -0800

    GitHub actions: for now only enable go on Ubuntu 22.04

--- travis/script.sh
+++ travis/script.sh
@@ -26,8 +26,8 @@ set -ex
 
 DEPS_UBUNTU='g++ cmake git libboost-regex-dev libkrb5-dev libssl-dev'
 DEPS_UBUNTU=$DEPS_UBUNTU' libfuse-dev default-jdk zlib1g-dev unzip maven sudo'
-DEPS_UBUNTU=$DEPS_UBUNTU' passwd curl openssl fuse gdb golang-go'
-DEPS_UBUNTU22=$DEPS_UBUNTU
+DEPS_UBUNTU=$DEPS_UBUNTU' passwd curl openssl fuse gdb'
+DEPS_UBUNTU22=$DEPS_UBUNTU' golang-go'
 DEPS_UBUNTU=$DEPS_UBUNTU' python-dev'
 DEPS_DEBIAN=$DEPS_UBUNTU
 
